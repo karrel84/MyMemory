fbf8e2eb3962677d7f358e46cea7f5e0a17a2eca
karrel <karrel84@naver.com>
2017-11-29 20:18:40 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by bodyfriend_dev on 2017. 11. 28..
+//  Copyright © 2017년 karrel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
